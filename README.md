2c7585a155abaf3113646adb50e8e03ad946ed14
Masen Matthews <masenmatthews@gmail.com>
2019-03-12 20:24:11 -0700

    add initial HTML pages and layouts, link database, add log in feature

---
+++ README.md
@@ -0,0 +1,8 @@
+# Project 1
+
+Web Programming with Python and JavaScript
+
+## Setup steps
+export FLASK_APP=application.py
+export FLASK_DEBUG=1
+export DATABASE_URL=(database URL)
---
+++ application.py
@@ -0,0 +1,42 @@
+import os
+
+from flask import Flask, session, request, render_template
+from flask_session import Session
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
+
+app = Flask(__name__)
+
+# Check for environment variable
+if not os.getenv("DATABASE_URL"):
+    raise RuntimeError("DATABASE_URL is not set")
+
+# Configure session to use filesystem
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+# Set up database
+engine = create_engine(os.getenv("DATABASE_URL"))
+db = scoped_session(sessionmaker(bind=engine))
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/login", methods=["POST"])
+def signup():
+    # Get login form information.
+    username = request.form.get("username")
+    password = request.form.get("password") 
+    # except ValueError:
+    #     return render_template("error.html", message="Invalid username or password")
+
+    # Makes sure that user doesn't already exist
+    # if db.execute("SELECT * FROM users WHERE username = :username"):
+    #     return render_template ("error.html")
+
+    db.execute("INSERT INTO users (username, password) VALUES (:username, :password)",
+               {"username": username, "password": password})
+    db.commit()
+    return render_template("success.html")
