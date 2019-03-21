cd7652883e380a5faf40e9265fc672993b201bb8
Masen Matthews <masenmatthews@gmail.com>
2019-03-21 10:35:19 -0700

    fix bugs, add imports, fix views for signup and login form

--- README.md
+++ README.md
@@ -5,4 +5,26 @@ Web Programming with Python and JavaScript
 ## Setup steps
 export FLASK_APP=application.py
 export FLASK_DEBUG=1
-export DATABASE_URL=(database URL)
+export DATABASE_URL=postgres://nojnxjyrnfsudn:e1caa2c6269e1abdd28e9a7b07fb8813a855656ca98231cece47fb786fba6e13@ec2-54-197-232-203.compute-1.amazonaws.com:5432/d50ghl4mrjj7h
+
+https://pythonspot.com/login-authentication-with-flask/
+## 
+
+    # Get signup form information.
+    # username = request.form.get("username")
+    # password = request.form.get("password") 
+
+    # Makes sure that user doesn't already exist
+    # if db.execute("SELECT * FROM users WHERE username=username"):
+    #     return render_template ("error.html")
+
+    # Add new user to database
+    # db.execute("INSERT INTO users (username, password) VALUES (:username, :password)",
+    #            {"username": username, "password": password})
+    # db.commit()
+
+    # if request.form['password'] == 'password' and request.form['username'] == 'admin':
+    #     session['logged_in'] = True
+    # else:
+    #     flash('wrong password!')
+    # return index()
--- application.py
+++ application.py
@@ -1,12 +1,16 @@
 import os
 
-from flask import Flask, session, request, render_template
+from flask import Flask, session, request, render_template, flash
 from flask_session import Session
+from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
+from wtforms import StringField, PasswordField, BooleanField
+from wtforms.validators import InputRequired, Email, Length
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 
 app = Flask(__name__)
-
 # Check for environment variable
 if not os.getenv("DATABASE_URL"):
     raise RuntimeError("DATABASE_URL is not set")
@@ -14,33 +18,65 @@ if not os.getenv("DATABASE_URL"):
 # Configure session to use filesystem
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
+app.config["SECRET_KEY"] = 'secret'
+Bootstrap(app)
 Session(app)
 
 # Set up database
 engine = create_engine(os.getenv("DATABASE_URL"))
 db = scoped_session(sessionmaker(bind=engine))
 
-@app.route("/")
+class LoginForm(FlaskForm):
+    username= StringField('username', validators=[InputRequired(), Length(min=4, max=15)])
+    password = PasswordField('password', validators=[InputRequired(), Length(min=8, max=80)])
+    remember = BooleanField('Remember me')
+
+class RegisterForm(FlaskForm):
+    email = StringField('email', validators=[InputRequired(), Email(message='Invalid email'), Length(max=50)])
+    username = StringField('username', validators=[InputRequired(), Length(min=4, max=15)])
+    password = PasswordField('password', validators=[InputRequired(), Length(min=8, max=80)])
+
+@app.route("/", methods=["GET", "POST"])
 def index():
-    return render_template("index.html")
+    return render_template('index.html')
 
-@app.route("/signup", methods=["POST"])
-def signup():
-    # Get signup information.
-    username = request.form.get("username")
-    password = request.form.get("password") 
-
-    # Makes sure that user doesn't already exist
-    if db.execute("SELECT * FROM users WHERE username=username"):
-        return render_template ("error.html")
-
-
-    db.execute("INSERT INTO users (username, password) VALUES (:username, :password)",
-               {"username": username, "password": password})
-    db.commit()
-    return render_template("success.html")
-
-
-@app.route("/login", methods=["GET"])
+@app.route("/login", methods=['GET', 'POST'])
 def login():
-    return render_template("login.html")
+    form = LoginForm()
+
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user:
+            if check_password_hash(user.password, form.password.data):
+                login_user(user, remember=form.remember.data)
+                return redirect(url_for('dashboard'))
+
+        return '<h1>Invalid username or password</h1>'
+        #return '<h1>' + form.username.data + ' ' + form.password.data + '</h1>'
+
+    return render_template('login.html', form=form)
+
+@app.route("/signup", methods=["GET", "POST"])
+def signup():
+    form = RegisterForm()
+
+    if form.validate_on_submit():
+        hashed_password = generate_password_hash(
+            form.password.data, method='sha256')
+        new_user = User(username=form.username.data,
+                        email=form.email.data, password=hashed_password)
+        db.session.add(new_user)
+        db.session.commit()
+
+        return '<h1>New user has been created!</h1>'
+        #return '<h1>' + form.username.data + ' ' + form.email.data + ' ' + form.password.data + '</h1>'
+
+    return render_template('signup.html', form=form)
+
+@app.route('/logout')
+def logout():
+    session['logged_in'] = False
+    return index()
+
+if __name__ == '__main__':
+    app.run(debug=True)
