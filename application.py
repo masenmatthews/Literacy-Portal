680d4496e9145c88057a4e546209b4757a84caa2
Masen Matthews <masenmatthews@gmail.com>
2019-03-23 16:43:49 -0700

    revert to commit 8580072936ccb64262e93a513bfa37ef6f045312

--- application.py
+++ application.py
@@ -1,16 +1,12 @@
 import os
 
-from flask import Flask, session, request, render_template, flash
+from flask import Flask, session, request, render_template
 from flask_session import Session
-from flask_bootstrap import Bootstrap
-from flask_wtf import FlaskForm
-from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
-from wtforms import StringField, PasswordField, BooleanField
-from wtforms.validators import InputRequired, Email, Length
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 
 app = Flask(__name__)
+
 # Check for environment variable
 if not os.getenv("DATABASE_URL"):
     raise RuntimeError("DATABASE_URL is not set")
@@ -18,65 +14,33 @@ if not os.getenv("DATABASE_URL"):
 # Configure session to use filesystem
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
-app.config["SECRET_KEY"] = 'secret'
-Bootstrap(app)
 Session(app)
 
 # Set up database
 engine = create_engine(os.getenv("DATABASE_URL"))
 db = scoped_session(sessionmaker(bind=engine))
 
-class LoginForm(FlaskForm):
-    username= StringField('username', validators=[InputRequired(), Length(min=4, max=15)])
-    password = PasswordField('password', validators=[InputRequired(), Length(min=8, max=80)])
-    remember = BooleanField('Remember me')
-
-class RegisterForm(FlaskForm):
-    email = StringField('email', validators=[InputRequired(), Email(message='Invalid email'), Length(max=50)])
-    username = StringField('username', validators=[InputRequired(), Length(min=4, max=15)])
-    password = PasswordField('password', validators=[InputRequired(), Length(min=8, max=80)])
-
-@app.route("/", methods=["GET", "POST"])
+@app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
 
-@app.route("/login", methods=['GET', 'POST'])
-def login():
-    form = LoginForm()
-
-    if form.validate_on_submit():
-        user = User.query.filter_by(username=form.username.data).first()
-        if user:
-            if check_password_hash(user.password, form.password.data):
-                login_user(user, remember=form.remember.data)
-                return redirect(url_for('dashboard'))
-
-        return '<h1>Invalid username or password</h1>'
-        #return '<h1>' + form.username.data + ' ' + form.password.data + '</h1>'
-
-    return render_template('login.html', form=form)
-
-@app.route("/signup", methods=["GET", "POST"])
+@app.route("/signup", methods=["POST"])
 def signup():
-    form = RegisterForm()
+    # Get signup information.
+    username = request.form.get("username")
+    password = request.form.get("password") 
 
-    if form.validate_on_submit():
-        hashed_password = generate_password_hash(
-            form.password.data, method='sha256')
-        new_user = User(username=form.username.data,
-                        email=form.email.data, password=hashed_password)
-        db.session.add(new_user)
-        db.session.commit()
+    # Makes sure that user doesn't already exist
+    if db.execute("SELECT * FROM users WHERE username=username"):
+        return render_template ("error.html")
 
-        return '<h1>New user has been created!</h1>'
-        #return '<h1>' + form.username.data + ' ' + form.email.data + ' ' + form.password.data + '</h1>'
 
-    return render_template('signup.html', form=form)
+    db.execute("INSERT INTO users (username, password) VALUES (:username, :password)",
+               {"username": username, "password": password})
+    db.commit()
+    return render_template("success.html")
 
-@app.route('/logout')
-def logout():
-    session['logged_in'] = False
-    return index()
 
-if __name__ == '__main__':
-    app.run(debug=True)
+@app.route("/login", methods=["GET"])
+def login():
+    return render_template("login.html")
